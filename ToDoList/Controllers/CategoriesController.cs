697ced6a45ebbba629f59b80bab52a719f10c793
hmcvay <hayleymcvay8@gmail.com>
2022-03-03 09:47:19 -0800

    Add views and routes for adding new category w/ hw

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      return RedirectToAction("Index");
+    }
+  }
+}
