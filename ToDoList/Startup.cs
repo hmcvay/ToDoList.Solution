6b98fdbfa0c8e4ed397a229f13c6a606c932b229
hmcvay <hayleymcvay8@gmail.com>
2022-03-07 13:14:08 -0800

    add config to sql database from hw.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -13,10 +13,6 @@ namespace ToDoList {
       Configuration = builder.Build();
     }
 
-    public static class DBConfiguration {
-      public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
-    }
-
     public IConfigurationRoot Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services) {
@@ -34,5 +30,9 @@ namespace ToDoList {
         await context.Response.WriteAsync("Hello World!");
       });
     }
+  }
+  public static class DBConfiguration 
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
   }
 }
