e15cd2cbb3cc58a779d211e0cb8827812e29e84d
hmcvay <hayleymcvay8@gmail.com>
2022-03-20 18:09:29 -0700

    Add Identity config along with hw.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -4,11 +4,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
 using ToDoList.Models;
 
-namespace ToDoList {
-  public class Startup {
+using Microsoft.AspNetCore.Identity;
+
+namespace ToDoList 
+{
+  public class Startup 
+  {
     public Startup(IWebHostEnvironment env) 
     {
       var builder = new ConfigurationBuilder()
@@ -27,11 +30,21 @@ namespace ToDoList {
       services.AddEntityFrameworkMySql()
         .AddDbContext<ToDoListContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+                  .AddEntityFrameworkStores<ToDoListContext>()
+                  .AddDefaultTokenProviders();
 }
 
     public void Configure(IApplicationBuilder app) {
       app.UseDeveloperExceptionPage();
+
+      app.UseAuthentication();
+
       app.UseRouting();
+
+      app.UseAuthorization();
+      
       app.UseEndpoints(routes => {
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
